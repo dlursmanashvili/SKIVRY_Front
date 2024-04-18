9d2d9875cbb61dd718f036fe03590d2278d791ea
D.lursmanashvili <D.lursmanashvili@DESKTOP-608QM2E>
2024-04-18 17:51:18 +0400

    finish avtorisation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +16,10 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginobj: Login;
 
+  constructor(private http: HttpClient) {
+    this.loginobj = new Login();
+  }
+  onLogin() {
+    debugger;
+    this.http.get<any>(
+      'https://localhost:7061/user/login',
+      {
+        params: {
+          Email: this.loginobj.Email,
+          Password: this.loginobj.Password
+        }
+      }
+    )
+    .subscribe(
+      response => {
+        console.log('Login successful', response);
+      },
+      error => {
+        console.error('Error occurred while logging in:', error);
+      }
+    );
+  }
+  
+}
+export class Login {
+  public Email: string;
+  public Password: string;
+
+  constructor() {
+    this.Email = '';
+    this.Password = '';
+  }
+ 
 }
